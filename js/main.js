46b04e00e9f941740dcf5b06b4a4adc7c3ad02c0
Johirul Islam <54662396+JohirulIslamShaky@users.noreply.github.com>
2019-12-06 20:22:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 5,
+        autoplayTimeout: 3000,
+        dotsEach: true,
+    });
+});
